7ad2db1cde20a87b39aca8ecdcc2651b5df2a0a4
Fabian Reyes <cesarrm5@hotmail.com>
2021-08-09 16:46:20 -0500

    Inserta botones

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import react,{useState} from 'react';
 
 function App() {
+  const [nombre, setNombre] = useState('')
+
+  const llamaFabian = () =>{
+    setNombre("Fabian")
+  }
+
+  const llamaTesi = () =>{
+    setNombre("TESI")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Hola Mundo</h2>
+        <h2>{nombre}</h2>
+        <button onClick={llamaTesi}>TESI</button>
+        <button onClick={llamaFabian}>Fabian</button>
       </header>
     </div>
   );
